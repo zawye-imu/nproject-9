22043d69c967726dbce816f08cd7a721e2ff7247
zaye-imu <www.zawyehtetnaing@gmail.com>
2019-09-02 21:43:59 -0700

    Coding for final

--- src/test/java/com/project/group9/AppIntegrationTest.java
+++ src/test/java/com/project/group9/AppIntegrationTest.java
@@ -2,11 +2,6 @@ package com.project.group9;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-
-import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class AppIntegrationTest
 {
--- src/test/java/com/project/group9/test.java
+++ src/test/java/com/project/group9/test.java
@@ -2,11 +2,6 @@ package com.project.group9;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-
-import java.util.ArrayList;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class test
 {
@@ -16,6 +11,7 @@ public class test
     static void init()
     {
         app = new App();
+        app.connect("localhost:33060");
 
 
     }
